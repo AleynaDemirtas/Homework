51ebc4144dac929607f3c802a87eeec8f98b7d46
Aleyna Demirtaş <63663147+AleynaDemirtas@users.noreply.github.com>
2022-04-16 23:55:44 +0300

    Add files via upload

---
+++ Homework3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). Sonrasında kullanıcıdan n adet kelime girmesi isteyin. 
+            Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın. */
+
+            Console.WriteLine("Bir adet pozitif tam sayı giriniz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[] words = new string[n];
+            Console.WriteLine(n +" "+ "adet kelime giriniz:");
+
+            for(int i=0;i<n;i++)
+            {
+               words[i]= Console.ReadLine();
+            }
+
+            foreach (var item in words)
+            {
+                Console.Write(" " +item+" ");
+            }
+        }
+    }
+}
