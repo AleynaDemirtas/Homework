c18746fe219e40f710f615f2e66b640be42080e1
Aleyna Demirtaş <63663147+AleynaDemirtas@users.noreply.github.com>
2022-04-17 00:04:30 +0300

    Add files via upload

---
+++ homework4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. 
+            Cümledeki toplam kelime ve harf sayısını console'a yazdırın.*/
+
+            string[] words;
+
+            Console.WriteLine("Bir cümle giriniz:");
+            string clause=Console.ReadLine();
+
+            words = clause.Split(" ");
+
+            Console.WriteLine("Cümlede {0} kelime var",words.Length);
+             Console.WriteLine("Cümlede {0} harf var",clause.Length);
+
+        }
+    }
+}
